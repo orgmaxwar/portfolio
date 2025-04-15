4d726e8a1f639852c5705ea12cc36f8abb0a7e3c
makhmudh <makhmud.khojiev@theknowledgeacademy.uz>
2025-04-15 23:32:09 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Huzaif Ahmed",
+          to_name: "Makhmud",
           from_email: form.email,
           to_email: "hojievmakhmud@gmail.com",
           message: form.message,
